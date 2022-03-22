9d6d598ea8e19f0d6320f358c495e9c477d6f79f
cobby12 <cobbywilberforce@gmail.com>
2022-03-22 15:08:04 -0700

    'new'

--- regex.py
+++ regex.py
@@ -1,6 +1,7 @@
 import re
 
-def Check
+def Check	        	                	        	                            	                            	                                    	                                                                                case 'dark':
+	
 	        	                	        	                            	                            	                                    	                                                                    break;
 	        	                	        	                            	                            	                                    	                                                    document.documentElement.setAttribute('color-theme', 'system');
 	        	                	        	                            	                            	                                    	                                    }
