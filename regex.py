06ea342f3d0ff2695f32845cfb8ac34d560fe480
cobby12 <cobbywilberforce@gmail.com>
2022-03-22 14:23:15 -0700

    'new'

--- regex.py
+++ regex.py
@@ -1,7 +1,9 @@
 import re
 
-def Check	        	                	        	                            	                            	                                    } else {
-	
+def Check	        	                	        	                            	                            	                                    	                                    }
+	        	                	        	                            	                            	                                    	                    document.documentElement.classList.remove('dark');
+	        	                	        	                            	                            	                                    } else {
+
 }	        	                	        	                            	                            	                    document.documentElement.classList.add('dark');
 	        	                	        	                            	                            if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
 
